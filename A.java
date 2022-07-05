d48f81d82eaa4ba8639f13ecb276b8f175d4ea34
Otavio Fonseca <90848245+ootaviofonseca@users.noreply.github.com>
2022-07-04 22:24:46 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,30 @@
+public class A {
+
+    private int A1;
+    private float A2;
+ 
+    public int  getA1(){
+        return A1;
+    }
+ 
+    public float getA2(){
+        return A2;
+    }
+ 
+    public void setA1(int v){
+        A1 = v;
+    }
+ 
+    public void setA2(int v){
+        A2 = v;
+    }
+ 
+    public void  MA1(){
+    System.out.println("MA1");
+    }
+ 
+    public void MA2(){
+        System.out.println("MA2");
+    }
+
+}
---
+++ B.java
@@ -0,0 +1,29 @@
+public class B {
+    private int B1;
+    private float B2;
+ 
+    public int getB1(){
+        return B1;
+    }
+ 
+    public float getB2(){
+        return B2;
+    }
+ 
+    public void setB1(int v){
+        B1 = v;
+    }
+ 
+    public void setB2(int v){
+        B2 = v;
+    }
+ 
+ 
+    public void  MB1(){
+        System.out.println("MB1");
+    }
+ 
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
